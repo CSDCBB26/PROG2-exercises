835e10178bcb345a92ca398bed4c6781024eb28b
Andreas Drozd <andreas.drozd@stud.fh-campuswien.ac.at>
2024-04-06 14:42:51 +0200

    work on API Class and Test class

--- src/main/java/at/ac/fhcampuswien/fhmdb/utils/MovieAPI.java
+++ src/main/java/at/ac/fhcampuswien/fhmdb/utils/MovieAPI.java
@@ -1,5 +1,6 @@
 package at.ac.fhcampuswien.fhmdb.utils;
 
+import at.ac.fhcampuswien.fhmdb.Genre;
 import com.google.gson.Gson;
 import okhttp3.*;
 
@@ -9,14 +10,14 @@ public class MovieAPI {
     public static final String API_URL = "https://prog2.fh-campuswien.ac.at";
 
     public static String getAllMovies(String api_url) {
+        OkHttpClient client = new OkHttpClient();
+
         Request request = new Request.Builder()
                 .url(api_url + "/movies")
                 .header("User-Agent", "http.agent")
                 .build();
 
-        OkHttpClient client = new OkHttpClient();
 
-        Call call = client.newCall(request);
         try (Response response = client.newCall(request).execute()) {
             if (response.body() != null) {
                 return response.body().string();
@@ -24,12 +25,40 @@ public class MovieAPI {
         } catch (Exception e) {
             return "Error: " + e.getMessage();
         }
-
         return null;
     }
 
-    public static void main(String[] args) {
-        System.out.println(getAllMovies(API_URL));    }
+    public static String getMoviesByQueries(String api_url, String query, Genre genre, int releaseYear, double ratingFrom) {
+        OkHttpClient client = new OkHttpClient();
 
+        HttpUrl.Builder urlBuilder = HttpUrl.parse(api_url + "/movies").newBuilder();
+        if (query != null) {
+            urlBuilder.addQueryParameter("query", query);
+        }
+        if (genre != null) {
+            urlBuilder.addQueryParameter("genre", genre.name());
+        }
+        if (releaseYear != 0) {
+            urlBuilder.addQueryParameter("releaseYear", String.valueOf(releaseYear));
+        }
+        if (ratingFrom != 0) {
+            urlBuilder.addQueryParameter("ratingFrom", String.valueOf(ratingFrom));
+        }
 
+        String url = urlBuilder.build().toString();
+
+        Request request = new Request.Builder()
+                .url(url)
+                .header("User-Agent", "http.agent")
+                .build();
+
+        try (Response response = client.newCall(request).execute()) {
+            if (response.body() != null) {
+                return response.body().string();
+            }
+        } catch (Exception e) {
+            return "Error: " + e.getMessage();
+        }
+        return null;
+    }
 }
