258df49396699dc7af4512d09d59d4303d941cb6
Sergiu Iordanescu <iordanescu.sergiu@gmail.com>
2024-04-10 15:09:33 +0200

    Add tests for longest movie title

--- src/main/test/at/ac/fhcampuswien/fhmdb/GetLongestMovieTitleTest.java
+++ src/main/test/at/ac/fhcampuswien/fhmdb/GetLongestMovieTitleTest.java
@@ -1,7 +1,60 @@
 package at.ac.fhcampuswien.fhmdb;
 
-public class GetLongestMovieTitleTest {
+import at.ac.fhcampuswien.fhmdb.models.Movie;
+import at.ac.fhcampuswien.fhmdb.utils.MovieUtils;
+import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class GetLongestMovieTitleTest extends BaseTest {
+
+    @Test
+    void longest_movie_title_with_all_movies_given_should_return_46() {
+        // Star Wars: Episode V - The Empire Strikes Back
+        long result = MovieUtils.getLongestMovieTitle(movieList);
+        assertEquals(46, result);
+    }
+
+    @Test
+    void longest_movie_title_with_list_of_movies_containing_The_Godfather_and_Pulp_Fiction_should_return_13() {
+        long result = MovieUtils.getLongestMovieTitle(
+                List.of(movieMap.get("Pulp Fiction"), movieMap.get("The Godfather"))
+        );
+        assertEquals(13, result);
+    }
+
+    @Test
+    void longest_movie_title_with_list_of_movies_containing_The_Shawshank_Redemption_Once_Upon_a_Time_in_Hollywood_12_Angry_Men_should_return_29() {
+        long result = MovieUtils.getLongestMovieTitle(
+                List.of(movieMap.get("The Shawshank Redemption"),
+                        movieMap.get("Once Upon a Time in Hollywood"),
+                        movieMap.get("12 Angry Men"))
+        );
+        assertEquals(29, result);
+    }
+
+    @Test
+    void longest_movie_title_with_list_containing_a_movie_with_no_title_should_return_0() {
+        Movie noTitleMovie = new Movie.Builder()
+                .setTitle("")
+                .build();
+        long result = MovieUtils.getLongestMovieTitle(List.of(noTitleMovie));
+        assertEquals(0, result);
+    }
+
+    @Test
+    void longest_movie_title_with_empty_list_should_return_0() {
+        long result = MovieUtils.getLongestMovieTitle(new ArrayList<>());
+        assertEquals(0, result);
+    }
+
+    @Test
+    void longest_movie_title_with_null_list_should_return_0() {
+        long result = MovieUtils.getLongestMovieTitle(null);
+        assertEquals(0, result);
+    }
 
 }
