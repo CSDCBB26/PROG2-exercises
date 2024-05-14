e8daa693e35b467d0ab4439ac8221193ce20a258
Sergiu Iordanescu <iordanescu.sergiu@gmail.com>
2024-05-14 09:54:54 +0200

    Create WatchlistMovieEntity class

--- src/main/java/at/ac/fhcampuswien/fhmdb/database/WatchlistMovieEntity.java
+++ src/main/java/at/ac/fhcampuswien/fhmdb/database/WatchlistMovieEntity.java
@@ -1,4 +1,39 @@
 package at.ac.fhcampuswien.fhmdb.database;
 
+import com.j256.ormlite.field.DatabaseField;
+import com.j256.ormlite.table.DatabaseTable;
+
+@DatabaseTable(tableName = "watchlist_movies")
 public class WatchlistMovieEntity {
+
+    @DatabaseField(id = true)
+    private long id;
+
+    @DatabaseField(canBeNull = false)
+    private String apiId;
+
+    // DO NOT REMOVE - ORMLite requires a no-arg constructor
+    public WatchlistMovieEntity() {
+    }
+
+    public WatchlistMovieEntity(long id, String apiId) {
+        this.id = id;
+        this.apiId = apiId;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getApiId() {
+        return apiId;
+    }
+
+    public void setApiId(String apiId) {
+        this.apiId = apiId;
+    }
 }
