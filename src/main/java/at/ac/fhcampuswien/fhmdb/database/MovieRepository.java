c82a177de1abd029947456f12a677cf227a3290e
Sergiu Iordanescu <iordanescu.sergiu@gmail.com>
2024-05-14 09:54:34 +0200

    Create MovieRepository class

--- src/main/java/at/ac/fhcampuswien/fhmdb/database/MovieRepository.java
+++ src/main/java/at/ac/fhcampuswien/fhmdb/database/MovieRepository.java
@@ -1,4 +1,35 @@
 package at.ac.fhcampuswien.fhmdb.database;
 
+import com.j256.ormlite.dao.Dao;
+
+import java.sql.SQLException;
+import java.util.List;
+
 public class MovieRepository {
+
+    private Dao<MovieEntity, Long> dao;
+
+    public MovieRepository(Dao<MovieEntity, Long> dao) {
+        this.dao = dao;
+    }
+
+    public List<MovieEntity> getAllMovies() throws SQLException {
+        return dao.queryForAll();
+    }
+
+    public int removeAll() throws SQLException {
+        return dao.deleteBuilder().delete();
+    }
+
+    public MovieEntity getMovie(Long id) throws SQLException {
+        return dao.queryForId(id);
+    }
+
+    public int addAllMovies(List<MovieEntity> movies) throws SQLException {
+        int count = 0;
+        for (MovieEntity movie : movies) {
+            count += dao.createOrUpdate(movie).getNumLinesChanged();
+        }
+        return count;
+    }
 }
