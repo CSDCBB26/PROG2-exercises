4944b071d400b547044a560b12f80a9cf1268618
Sergiu Iordanescu <iordanescu.sergiu@gmail.com>
2024-05-14 09:44:58 +0200

    Create MovieEntity class

--- src/main/java/at/ac/fhcampuswien/fhmdb/database/MovieEntity.java
+++ src/main/java/at/ac/fhcampuswien/fhmdb/database/MovieEntity.java
@@ -1,4 +1,87 @@
 package at.ac.fhcampuswien.fhmdb.database;
 
+import com.j256.ormlite.field.DatabaseField;
+import com.j256.ormlite.table.DatabaseTable;
+
+@DatabaseTable(tableName = "movies")
 public class MovieEntity {
+
+    @DatabaseField(id = true)
+    private String id;
+
+    @DatabaseField(canBeNull = false)
+    private String title;
+
+    @DatabaseField
+    private String description;
+
+    @DatabaseField
+    private String genres; // Stored as a comma-separated string
+
+    @DatabaseField
+    private int year;
+
+    @DatabaseField
+    private double rating;
+
+    // DO NOT REMOVE - ORMLite requires a no-arg constructor
+    public MovieEntity() {
+    }
+
+    public MovieEntity(String id, String title, String description, String genres, int year, double rating) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.genres = genres;
+        this.year = year;
+        this.rating = rating;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getGenres() {
+        return genres;
+    }
+
+    public void setGenres(String genres) {
+        this.genres = genres;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public void setRating(double rating) {
+        this.rating = rating;
+    }
 }
