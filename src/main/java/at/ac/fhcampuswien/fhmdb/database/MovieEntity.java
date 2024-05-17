9ab3c2acd658408e57b53a26bbe8892e7de1ece8
Sergiu Iordanescu <iordanescu.sergiu@gmail.com>
2024-05-17 11:13:05 +0200

    Adapt MovieEntity class: create required methods and use Builder pattern

--- src/main/java/at/ac/fhcampuswien/fhmdb/database/MovieEntity.java
+++ src/main/java/at/ac/fhcampuswien/fhmdb/database/MovieEntity.java
@@ -1,13 +1,21 @@
 package at.ac.fhcampuswien.fhmdb.database;
 
+import at.ac.fhcampuswien.fhmdb.Genre;
+import at.ac.fhcampuswien.fhmdb.models.Movie;
 import com.j256.ormlite.field.DatabaseField;
 import com.j256.ormlite.table.DatabaseTable;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @DatabaseTable(tableName = "movies")
 public class MovieEntity {
 
-    @DatabaseField(id = true)
-    private String id;
+    @DatabaseField(generatedId = true)
+    private long id;
+
+    @DatabaseField(canBeNull = false)
+    private String apiId;
 
     @DatabaseField(canBeNull = false)
     private String title;
@@ -19,7 +27,16 @@ public class MovieEntity {
     private String genres;
 
     @DatabaseField
-    private int year;
+    private String directors;
+
+    @DatabaseField
+    private int releaseYear;
+
+    @DatabaseField
+    private String imgUrl;
+
+    @DatabaseField
+    private int lengthInMinutes;
 
     @DatabaseField
     private double rating;
@@ -28,21 +45,32 @@ public class MovieEntity {
     public MovieEntity() {
     }
 
-    public MovieEntity(String id, String title, String description, String genres, int year, double rating) {
-        this.id = id;
-        this.title = title;
-        this.description = description;
-        this.genres = genres;
-        this.year = year;
-        this.rating = rating;
+    private MovieEntity(Builder builder) {
+        this.apiId = builder.apiId;
+        this.title = builder.title;
+        this.description = builder.description;
+        this.genres = listToString(builder.genres);
+        this.directors = listToString(builder.directors);
+        this.releaseYear = builder.releaseYear;
+        this.imgUrl = builder.imgUrl;
+        this.lengthInMinutes = builder.lengthInMinutes;
+        this.rating = builder.rating;
     }
 
-    public String getId() {
+    public long getId() {
         return id;
     }
 
-    public void setId(String id) {
+    public void setId(long id) {
         this.id = id;
+    }
+
+    public String getApiId() {
+        return apiId;
+    }
+
+    public void setApiId(String apiId) {
+        this.apiId = apiId;
     }
 
     public String getTitle() {
@@ -69,12 +97,36 @@ public class MovieEntity {
         this.genres = genres;
     }
 
-    public int getYear() {
-        return year;
+    public String getDirectors() {
+        return directors;
     }
 
-    public void setYear(int year) {
-        this.year = year;
+    public void setDirectors(String directors) {
+        this.directors = directors;
+    }
+
+    public int getReleaseYear() {
+        return releaseYear;
+    }
+
+    public void setReleaseYear(int releaseYear) {
+        this.releaseYear = releaseYear;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+
+    public int getLengthInMinutes() {
+        return lengthInMinutes;
+    }
+
+    public void setLengthInMinutes(int lengthInMinutes) {
+        this.lengthInMinutes = lengthInMinutes;
     }
 
     public double getRating() {
@@ -83,5 +135,112 @@ public class MovieEntity {
 
     public void setRating(double rating) {
         this.rating = rating;
+    }
+
+    public static <T> String listToString(List<T> list) {
+        return list.stream().map(Object::toString).collect(Collectors.joining(","));
+    }
+
+    public static String genresToString(List<Genre> genres) {
+        return listToString(genres);
+    }
+
+    public static String directorsToString(List<String> directors) {
+        return listToString(directors);
+    }
+
+    public static List<MovieEntity> fromMovies(List<Movie> movies) {
+        return movies.stream()
+                .map(movie -> new MovieEntity.Builder()
+                        .setApiID(movie.getAppID())
+                        .setTitle(movie.getTitle())
+                        .setDescription(movie.getDescription())
+                        .setGenres(movie.getGenres())
+                        .setDirectors(movie.getDirectors())
+                        .setReleaseYear(movie.getReleaseYear())
+                        .setImgUrl(movie.getImgUrl())
+                        .setLengthInMinutes(movie.getLengthInMinutes())
+                        .setRating(movie.getRating())
+                        .build()
+                )
+                .collect(Collectors.toList());
+    }
+
+    public static List<Movie> toMovies(List<MovieEntity> movieEntities) {
+        return movieEntities.stream()
+                .map(entity -> new Movie.Builder()
+                        .setAppID(entity.getApiId())
+                        .setTitle(entity.getTitle())
+                        .setDescription(entity.getDescription())
+                        .setGenres(Genre.stringToGenres(entity.getGenres()))
+                        .setDirectors(List.of(entity.getDirectors().split(",")))
+                        .setReleaseYear(entity.getReleaseYear())
+                        .setimgUrl(entity.getImgUrl())
+                        .setLengthInMinutes(entity.getLengthInMinutes())
+                        .setRating(entity.getRating())
+                        .build()
+                )
+                .collect(Collectors.toList());
+    }
+
+    public static class Builder {
+        private String apiId;
+        private String title;
+        private String description;
+        private List<Genre> genres;
+        private int releaseYear;
+        private String imgUrl;
+        private int lengthInMinutes;
+        private List<String> directors;
+        private double rating;
+
+        public Builder setApiID(String apiId) {
+            this.apiId = apiId;
+            return this;
+        }
+
+        public Builder setTitle(String title) {
+            this.title = title;
+            return this;
+        }
+
+        public Builder setDescription(String description) {
+            this.description = description;
+            return this;
+        }
+
+        public Builder setGenres(List<Genre> genres) {
+            this.genres = genres;
+            return this;
+        }
+
+        public Builder setImgUrl(String imgUrl) {
+            this.imgUrl = imgUrl;
+            return this;
+        }
+
+        public Builder setReleaseYear(int releaseYear) {
+            this.releaseYear = releaseYear;
+            return this;
+        }
+
+        public Builder setLengthInMinutes(int lengthInMinutes) {
+            this.lengthInMinutes = lengthInMinutes;
+            return this;
+        }
+
+        public Builder setDirectors(List<String> directors) {
+            this.directors = directors;
+            return this;
+        }
+
+        public Builder setRating(double rating) {
+            this.rating = rating;
+            return this;
+        }
+
+        public MovieEntity build() {
+            return new MovieEntity(this);
+        }
     }
 }
